2a8a038053aa30b6202e8c53dc3f46bbc0f25825
dinh2020hieu2001 <dinh1199hieu@gmail.com>
2020-12-01 20:57:24 +0700

    update new file

---
+++ Untitled1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Point {
+private :
+	double x;
+	double y;
+public :
+	Point (){
+		cin >> x;
+		cin >> y;
+	}
+	
+	Point (double x, double y){
+		this->x = x;
+		this->y = y;
+	}
+	
+	void setX(double x){
+		this->x= x;
+	}
+	
+	void setY(double y){
+		this->y = y;
+	}
+	
+	double getX(){
+		return x;
+	}
+	
+	double getY(){
+		return y;
+	}
+	
+	void setXY(double x, double y){
+		this->x = x;
+		this->y = y;
+	}
+	
+	double distance(double x, double y){
+		return sqrt((x-this->x)*(x-this->x) + (y -this->y)*(y -this->y));
+	}
+	
+	double distance (Point another){
+		return  sqrt((another.getX()-this->x)*(another.getX()-this->x) + (another.getY()-this->y)*(another.getY()-this->y));
+	}
+};
+
